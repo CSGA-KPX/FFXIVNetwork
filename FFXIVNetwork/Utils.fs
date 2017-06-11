984991b9f9b7b87e7e7c972484aed153d14753a5
KPX <dm.mikufans@gmail.com>
2017-06-11 15:22:46 +0800

    隐藏HexTable

--- FFXIVNetwork/Utils.fs
+++ FFXIVNetwork/Utils.fs
@@ -13,7 +13,7 @@ type HexString =
       |> Array.ofSeq
 
     // from https://blogs.msdn.microsoft.com/blambert/2009/02/22/blambertcodesnip-fast-byte-array-to-hex-string-conversion/
-    static member HexTable =
+    static member private HexTable =
         [|  "00"; "01"; "02"; "03"; "04"; "05"; "06"; "07"; "08"; "09"; "0A"; "0B"; "0C"; "0D"; "0E"; "0F";
             "10"; "11"; "12"; "13"; "14"; "15"; "16"; "17"; "18"; "19"; "1A"; "1B"; "1C"; "1D"; "1E"; "1F";
             "20"; "21"; "22"; "23"; "24"; "25"; "26"; "27"; "28"; "29"; "2A"; "2B"; "2C"; "2D"; "2E"; "2F";
